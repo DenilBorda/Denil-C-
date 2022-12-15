6b9fb47584b7e8d391699a084665e08837c6de99
Denil Borda <jigarchovatiya12@gmail.com>
2022-12-15 19:13:03 +0530

    15-12-22

---
+++ constructor_overloading.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class test
+{
+    public:
+    test()
+    {
+        cout<<"default con. is called..."<<endl;
+    }
+    test(int j)
+    {
+        cout<<"parametarzied called j :- "<<j<<endl;
+    }
+    test(int x, int y)
+    {
+        cout<<"parametarzied called x+y :- "<<x+y<<endl;
+    }
+    test(int a, int b, int c)
+    {
+        cout<<"parametarzied called a+b+c :- "<<a+b+c<<endl;
+    }
+};
+int main()
+{
+    test a,b(10),c(10,20),d(10,20,30);
+}
