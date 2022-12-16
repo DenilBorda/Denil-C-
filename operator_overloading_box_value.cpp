4ba172559745ee2a4155ea15de553b480b12e8d3
Denil Borda <jigarchovatiya12@gmail.com>
2022-12-16 19:44:16 +0530

    16-12-22

--- operator_overloading_box_value.cpp
+++ operator_overloading_box_value.cpp
@@ -2,5 +2,40 @@
 using namespace std;
 class box
 {
-
+    int length,breath,height;
+    public:
+    int setdata(int l,int b,int h);
+    void getdata();
+    box operator*(box &d);
 };
+int box :: setdata(int l,int b,int h)
+{
+    length = l;
+    breath = b;
+    height = h;
+    return l*b*h;
+}
+box box :: operator*(box &d)
+{
+    box r;
+    r.length = length *d.length;
+    r.breath = breath *d.breath;
+    r.height = height *d.height;
+    return r;
+}
+void box :: getdata()
+{
+    cout<<"Length :"<<length<<endl;
+    cout<<"Breath :"<<breath<<endl;
+    cout<<"Height :"<<height<<endl;
+}
+int main()
+{
+    box a,b,c;
+    a.setdata(2,2,2);
+    a.getdata();
+    b.setdata(4,4,4);
+    b.getdata();
+    c = a * b ;
+    c.getdata();
+}
